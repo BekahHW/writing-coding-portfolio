65abc9d4baf51b7937aba0c1ff0914d4de86dac7
github username <github email address>
2019-05-28 05:43:45 -0400

    Add Venn Diagragm, react router to app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import NavBar from '../components/NavBar'
+import Home from '../components/Home'
+import Writing from './Writing'
+import Coding from './Coding'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
 
-export default App;
+    <Router>
+      <React.Fragment>
+        <NavBar />
+        <Route exact path="/" component={Home} />
+
+        <Route exact path="/writing" component={Writing} />
+        <Route exact path="/coding" component={Coding} />
+
+      </React.Fragment>
+    </Router>
+
+  </div>
+)
+
+export default App
