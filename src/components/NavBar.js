d192699fcd87c4585c84b550bbefb632613cfe86
github username <github email address>
2019-05-30 16:14:18 -0400

    Add logo to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,10 +8,15 @@ import Button from '@material-ui/core/Button';
 import { NavLink } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import { SocialIcon } from 'react-social-icons';
+import mainLogo from '../images/lifecode_trans.png'
 
 
 
 const styles = {
+  navBar:{
+    background: 'purple',
+  },
+
   root: {
     flexGrow: 1,
 
@@ -27,7 +32,14 @@ const styles = {
 
   socialIcon: {
     margin: 10,
-  }
+  },
+
+  logo: {
+    width: 120,
+    height: 100,
+    padding: 0,
+    margin: 0
+  },
 
 };
 
@@ -35,15 +47,15 @@ const NavBar = (props) => {
   const { classes } = props;
 
   return <div className={classes.root}>
-    <AppBar position="static">
-      <Toolbar>
+    <AppBar position="static" >
+      <Toolbar className={classes.navBar}>
 
 
         <Typography
           variant="h6" className={classes.grow}>
           <NavLink style={{ color: 'white', textDecoration: 'none' }}
             to="/">
-            Logo
+            <img src={mainLogo} className={classes.logo} alt="watercolorComputer" />
             </NavLink>
         </Typography>
         <SocialIcon className={classes.socialIcon} url="http://twitter.com/bekahhw" />
