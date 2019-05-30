dbe45312e31075b1b9f4ffeb18d9641e701aa548
github username <github email address>
2019-05-30 06:33:07 -0400

    Add memoir component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import NavBar from './components/NavBar'
 import Writing from './components/Writing'
 import Coding from './components/Coding'
+import VennDiagram from './components/VennDiagram'
+
 
 const App = () => (
   <div className="App">
