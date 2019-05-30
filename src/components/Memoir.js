8c01f59719ee928403142cb32fa6ad41574f084b
github username <github email address>
2019-05-30 06:33:59 -0400

    Update memoir component with content

---
+++ src/components/Memoir.js
@@ -0,0 +1,8 @@
+import 'react' from React
+
+const Memoir = () => (
+  <div>
+    <h1>The Scars That Make Us Beautiful</h1>
+    <p>This trauma memoir explores the damage done when doctors don’t believe their patients, when questions are unanswered, and when no one wants to help. It tells of the power of community, empathy, and growth when you’re fighting to figure out who you are in your post-trauma world, and gives hope for a future where you are thriving, not just surviving. </p>
+  </div>
+)
