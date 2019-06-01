8f3ba2561b364cd69a6fbac04b62ba544f1092ac
github username <github email address>
2019-06-01 06:15:25 -0400

    Add About page

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const About = () => (
+  <h1>About</h1>
+  <p>After working as an adjunct English teacher for ten years, I experienced a trauma while having my fourth kid. After major surgery, I found coding calmed the trauma that was going on in my head. I could focus on the errors, on creating, on learning. I was hooked. So I left my adjunct position and with the Facebook + Flatiron Emerging Scholars Scholarship attended and completed Flatiron School Bootcamp.</p>
+  <p>My favorite parts of learning to code have been the problem-solving and community (as I also have a background in community organizing).</p>
+
+)
+
+export default About 
